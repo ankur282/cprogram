4a9a5f8427fa940df5c31366bf6704178b7f308f
ankur <ankur@ankur.com>
2023-03-01 18:22:20 +0530

    Initial commit

--- day28_02/day01-3/bst.c
+++ day28_02/day01-3/bst.c
@@ -25,6 +25,22 @@ struct node* insert_bst(struct node* root,int data)
 
 
     }
+    else
+    {
+        if (data<root->data)
+        {
+            // inster on left 
+            root->left=insert_bst(root->left,data);
+            /* code */
+        }
+        else
+        {
+            // insert on right
+            root->right=insert_bst(root->right,data);
+        }
+        
+    }
+    
     return root;
 }
 
@@ -41,16 +57,76 @@ struct node* take_input(struct node* root)
         scanf("%d",&data);   
         /* code */
     }
-    
-    
-    
-    return root;
+
+return root;
 
 }
+
+void display_inorder(struct node *root)
+{
+if(root==NULL){return;}
+display_inorder(root->left);
+printf("%d\n",root->data);
+display_inorder(root->right);
+}
+
+void display_pre(struct node *root)
+{
+if(root==NULL){return;}
+printf("%d\n",root->data);
+display_pre(root->left);
+display_pre(root->right);
+}
+
+void display_post(struct node *root)
+{
+if(root==NULL){return;}
+display_post(root->left);
+display_post(root->right);
+printf("%d\n",root->data);
+}
+
+int display_min(struct node *root)
+{
+if(root->left==NULL){
+    printf("%d\n",root->data);
+    return;
+    }
+display_min(root->left);
+// printf("%d\n",root->data);
+// display_inorder(root->right);
+}
+
+int display_max(struct node *root)
+{
+if(root->right==NULL){
+    printf("%d\n",root->data);
+    return;
+    }
+display_max(root->right);
+// printf("%d\n",root->data);
+// display_inorder(root->right);
+}
+
 
 void main()
 {
     struct node* root;
     root=take_input(root);
+
+    display_inorder(root);
+    printf("\n");
+
+    display_min(root);
+
+    display_max(root);
+
+    //display_pre(root);
+    
+    //display_post(root);
+
+    
+
+    
 
 }
