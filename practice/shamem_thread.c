d0d6cf27694ce4ac2fa5a2ab1370a1e4729f590b
ankurmataiya@gmail.com <ankurmatariya@gmail.com>
2023-06-20 14:37:43 +0530

    the shynchronization of the thread

--- practice/shamem_thread.c
+++ practice/shamem_thread.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<pthread.h>
 #include<sys/ipc.h>
 #include<sys/shm.h>
 #include<unistd.h>
 #include<string.h>
+#include<semaphore.h>
+
+sem_t s;
 
 
 void* writer(void *arg)
 {
+	sem_wait(&s);
+
 	key_t key = ftok("shmfile",65);
 	int shmid = shmget(key,2048,0666|IPC_CREAT);
 	char *msg = (char*)shmat(shmid,NULL,0);
@@ -17,14 +21,16 @@ void* writer(void *arg)
 	//scanf("%20s",msg);
 	gets(msg);
 
-	//printf("The message is:%s",msg);
+	printf("The message is:%s\n",msg);
 	shmdt(msg);
 	sleep(1);
+	sem_post(&s);
 
 }
 
 void* reader(void *arg)
 {
+	sem_wait(&s);
 	key_t key = ftok("shmfile",65);
 
 	int shmid = shmget(key,2048,0666);
@@ -34,6 +40,7 @@ void* reader(void *arg)
 
 	shmdt(msg);
 	shmctl(shmid,IPC_RMID,NULL);
+	sem_post(&s);
 
 }
 
@@ -43,6 +50,7 @@ int main()
 {
 
 	pthread_t t1,t2;
+	sem_init(&s,0,1);
 
 	pthread_create(&t1,NULL,writer,NULL);
 	pthread_create(&t2,NULL,reader,NULL);
